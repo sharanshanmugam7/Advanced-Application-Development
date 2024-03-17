1f752fbf6612c1f844b41852d457c2965cac5486
sharan <sharanshanmugam7@gmail.com>
2024-03-17 20:07:35 +0530

    updated Day-3

--- Day-3/ezpay/src/components/Header/Header.jsx
+++ Day-3/ezpay/src/components/Header/Header.jsx
@@ -1,13 +1,11 @@
 import Navbar from "../Navbar/Navbar"
 import { NavLink } from 'react-router-dom';
 import "./Header.css";
-import Footer from "../Footer/Footer";
 const Header = () => {
   return (
     <header className="header">
        <span className="logo"><NavLink to="/">.ezPay</NavLink></span>
       <Navbar />
-      <Footer/>
     </header>
   ) 
 }
--- Day-3/ezpay/src/components/Register/Register.jsx
+++ Day-3/ezpay/src/components/Register/Register.jsx
@@ -1,10 +1,37 @@
-
+import "./Register.css"
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
+    <div className="register-container">
+    <section className="register-section">
+      <div className="register">
+        <p className="addr">
+          Manage your Mobile Recharge with the ezPay app
+        </p>
+        <ul className="recharge-options-r">
+          <li>Postpaid</li>
+          <li>Prepaid</li>
+          <li>Addons</li>
+          <li>Others</li>
+        </ul>
+        <div className="r-container">
+          <div className="r-welcome">
+            <h1>Welcome to ezPay</h1>
+            <h4>Log in to unlock a world of digital experiences.</h4>
+            <form className="register-form">
+              <label htmlFor="mobile">Mobile Number:</label>
+                <input type="tel" id="mobile" name="mobile" pattern="[0-9]*" maxLength="10" required />
+              <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+              <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+              <button type="submit">Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
   )
 }
 
