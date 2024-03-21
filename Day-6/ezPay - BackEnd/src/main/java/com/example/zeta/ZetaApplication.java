099df19c9bab692f3c5c86ec98bc541b844380b5
sharan <sharanshanmugam7@gmail.com>
2024-03-21 10:56:08 +0530

    day - 6

---
+++ Day-6/ezPay - BackEnd/src/main/java/com/example/zeta/ZetaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZetaApplication.class, args);
+	}
+
+}
---
+++ Day-6/ezPay - BackEnd/src/test/java/com/example/zeta/ZetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
